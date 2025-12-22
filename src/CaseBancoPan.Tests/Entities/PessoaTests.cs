a672c11bdcebd59762724667065c3112a70aa852
Wellington <wellington.climaco9@gmail.com>
2025-12-22 18:47:51 -0300

    test: unit tests para validacao de email e data de nascimento

---
+++ src/CaseBancoPan.Tests/Entities/PessoaTests.cs
@@ -0,0 +1,38 @@
+using CaseBancoPan.API.Entities;
+
+namespace CaseBancoPan.Tests.Entities;
+
+public class PessoaTests
+{
+    [Theory]
+    [InlineData("15/01/2000",false)]
+    [InlineData("01/01/2000",true)]
+    void DeveVerificarMaioridade(string dataNascimento, bool expectedResult)
+    {
+        //arrange
+        DateTime dataNascimentoConverted = DateTime.Parse(dataNascimento);
+        DateTime hoje = new DateTime(2018,01,01);
+        
+        Pessoa pessoa = new Pessoa("nome", "endereco", "11957631250", "123456", dataNascimentoConverted);
+        //act
+        var result = pessoa.VerificarMaioridade(hoje);
+
+        //assert
+        Assert.Equal(expectedResult, result);
+    }
+
+    [Fact]
+    void DeveVerificarSeEmailEValido()
+    {
+        //arrange
+        bool expectedResult = true;
+        string email = "emailteste@gmail.com";
+        Pessoa pessoa = new Pessoa("nome", "endereco", "11957631250", email, new DateTime(2000,01,01));
+        
+        //act
+        var result = pessoa.ValidarEmail();
+
+        //assert
+        Assert.Equal(expectedResult,result);
+    }
+}
