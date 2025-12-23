a087825c983f12acbf8a4e90edc89e9369fc88fe
Wellington <wellington.climaco9@gmail.com>
2025-12-23 18:07:07 -0300

    feat: estrutura base endpoint de cadastro de pessoa

---
+++ src/CaseBancoPan.API/Controllers/PessoaController.cs
@@ -0,0 +1,27 @@
+using CaseBancoPan.API.Entities;
+using CaseBancoPan.API.Interface;
+using CaseBancoPan.API.Requests.PessoaRequests;
+using FluentResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaseBancoPan.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PessoaController : ControllerBase
+{
+    private readonly IPessoaService _pessoaService;
+
+    public PessoaController(IPessoaService pessoaService)
+    {
+        _pessoaService = pessoaService;
+    }
+    
+    [HttpPost]
+    [Route("/cadastrar")]
+    public async Task<IActionResult> Cadastrar(CadastrarPessoaRequest pessoa)
+    {
+        var result = await _pessoaService.Cadastrar(pessoa);
+        return Created($"/obterById/{result.Value.Id}", result.Value);
+    }
+}
