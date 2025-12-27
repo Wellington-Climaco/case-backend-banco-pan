08ad658a0e8723e2d9475a31d0398f05f8f8d486
Wellington <wellington.climaco9@gmail.com>
2025-12-27 12:34:17 -0300

    feat: response obj

---
+++ src/CaseBancoPan.API/Responses/PessoaResponses/ObterTodosResponse.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Win32;
+
+namespace CaseBancoPan.API.Responses.PessoaResponses
+{
+    public record ObterTodosRegistrosResponse(int PaginaAtual,int TamanhoPagina,int TotalPaginas,
+        int TotalRegistros,bool PossuiPaginaAnterior,bool PossuiPaginaSeguinte, IEnumerable<PessoaResponse> Registros);
+    
+}
