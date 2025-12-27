071bd8c52f46027ab892f13cd78f3339ba128d00
Wellington <wellington.climaco9@gmail.com>
2025-12-27 12:34:54 -0300

    feat: starta a aplicação populando o banco com alguns registros

--- src/CaseBancoPan.API/Extesions/ConfigServices.cs
+++ src/CaseBancoPan.API/Extesions/ConfigServices.cs
@@ -1,4 +1,5 @@
 using CaseBancoPan.API.ContextDb;
+using CaseBancoPan.API.Entities;
 using CaseBancoPan.API.Interface;
 using CaseBancoPan.API.Repository;
 using CaseBancoPan.API.Services;
@@ -39,6 +40,18 @@ public static class ConfigServices
         Console.WriteLine($"qtd migrations: {dbContext.Database.GetMigrations().Count()}");
 
         await dbContext.Database.EnsureDeletedAsync();
+        Thread.Sleep(5000);
         await dbContext.Database.EnsureCreatedAsync();
+
+
+        Pessoa[] pessoa =
+        {
+            new Pessoa("jose", "santos", "rua A", "11970707070", $"email1@email.com", new DateTime(2000, 01, 01)),
+            new Pessoa("ana", "souza", "rua B", "11970707070", $"email2@email.com", new DateTime(2000, 01, 01)),
+            new Pessoa("maria julia", "cardoso", "rua C", "11970707070", $"email3@email.com", new DateTime(2000, 01, 01)),
+            new Pessoa("felipe", "silva", "rua D", "11970707070", $"email4@email.com", new DateTime(2000, 01, 01))
+        };
+        dbContext.Pessoas.AddRange(pessoa);
+        dbContext.SaveChanges();
     }
 }
